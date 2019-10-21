8b83e17fa0b9ee5f37d5d85c020677095514b9b6
Larifari <s.sridharan@campus.lmu.de>
2019-10-22 00:00:42 +0200

    added xStream

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,42 @@ public class Main {
 
 
   public static void main(String[] args) {
-    List<Entry> list = new ArrayList<>();
-    list.add(new Entry("EIP", 1.7, 9));
-    list.add(new Entry("Promo", 3.3, 6));
-    list.add(new Entry("Algo", 3.7, 6));
-    list.add(new Entry("RA", 1, 6));
-    list.add(new Entry("BS", 1, 6));
-    list.add(new Entry("Rechnernetze", 1, 6));
-    list.add(new Entry("Digitale Medien", 1, 6));
-    list.add(new Entry("Medientechnik", 0, 6));
-    list.add(new Entry("Computergrafik", 1, 6));
-    list.add(new Entry("LinAlg", 2.7, 6));
-    list.add(new Entry("Timi", 2.3, 3));
-    list.add(new Entry("Seminar", 2, 3));
-    list.add(new Entry("Analysis", 4, 9));
-    list.add(new Entry("SEP", 1.3, 12));
-    list.add(new Entry("Psycho", 1.7, 6));
-    list.add(new Entry("UX2", 1.3, 6));
-    list.add(new Entry("Statistik", 2.3, 6));
-    list.add(new Entry("UX1", 1.3, 6));
-    list.add(new Entry("Zeichnen", 0, 3));
-    //list.add(new Entry("UX3", 0, 6));
-//    list.add(new Entry("Arbeit", 0, 3));
-//    list.add(new Entry("Fachschaft", 0, 3));
-//    list.add(new Entry("Englisch", 0, 3));
-//    list.add(new Entry("VPS", 0, 3));
-//    list.add(new Entry("Softwaretechnik", 2, 6));
-//    list.add(new Entry("Datenbanken", 2, 6));
-//    list.add(new Entry("It", 2, 6));
-//    list.add(new Entry("swh", 2, 6));
-//    list.add(new Entry("thema", 2, 6));
-//    list.add(new Entry("ba", 2, 15));
+    List<util.Entry> list = new ArrayList<>();
+    list.add(new util.Entry("EIP", 1.7, 9));
+    list.add(new util.Entry("Promo", 3.3, 6));
+    list.add(new util.Entry("Algo", 3.7, 6));
+    list.add(new util.Entry("RA", 1, 6));
+    list.add(new util.Entry("BS", 1, 6));
+    list.add(new util.Entry("Rechnernetze", 1, 6));
+    list.add(new util.Entry("Digitale Medien", 1, 6));
+    list.add(new util.Entry("Medientechnik", 0, 6));
+    list.add(new util.Entry("Computergrafik", 1, 6));
+    list.add(new util.Entry("LinAlg", 2.7, 6));
+    list.add(new util.Entry("Timi", 2.3, 3));
+    list.add(new util.Entry("Seminar", 2, 3));
+    list.add(new util.Entry("Analysis", 4, 9));
+    list.add(new util.Entry("SEP", 1.3, 12));
+    list.add(new util.Entry("Psycho", 1.7, 6));
+    list.add(new util.Entry("UX2", 1.3, 6));
+    list.add(new util.Entry("Statistik", 2.3, 6));
+    list.add(new util.Entry("UX1", 1.3, 6));
+    list.add(new util.Entry("Zeichnen", 0, 3));
+    //list.add(new util.Entry("UX3", 0, 6));
+//    list.add(new util.Entry("Arbeit", 0, 3));
+//    list.add(new util.Entry("Fachschaft", 0, 3));
+//    list.add(new util.Entry("Englisch", 0, 3));
+//    list.add(new util.Entry("VPS", 0, 3));
+//    list.add(new util.Entry("Softwaretechnik", 2, 6));
+//    list.add(new util.Entry("Datenbanken", 2, 6));
+//    list.add(new util.Entry("It", 2, 6));
+//    list.add(new util.Entry("swh", 2, 6));
+//    list.add(new util.Entry("thema", 2, 6));
+//    list.add(new util.Entry("ba", 2, 15));
 
     double gesamtECTS = 0;
     double ectsMitNote = 0;
     //Berechne GesamtEcts
-    for (Entry entry : list) {
+    for (util.Entry entry : list) {
       gesamtECTS += entry.getECTS();
       if (entry.hasNote()) {
         ectsMitNote += entry.getECTS();
@@ -51,10 +51,10 @@ public class Main {
     System.out.println(muellECTS);
     //Ziehe ECTS von den 16% schlechtesten Noten ab und lösche sie aus der Liste
     double max = 0;
-    Entry muellEntry = null;
+    util.Entry muellEntry = null;
     while (muellECTS > 0) {
 
-      for (Entry entry : list) {
+      for (util.Entry entry : list) {
         if (entry.getNote() > 0 && entry.getNote() > max) {
           muellEntry = entry;
           max = muellEntry.getNote();
@@ -68,7 +68,7 @@ public class Main {
 
 //stimmt
     double note = 0;
-    for (Entry entry : list) {
+    for (util.Entry entry : list) {
       note += entry.getNote() * entry.getECTS();
     }
 
--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -2,13 +2,18 @@ package gui;
 
 //TODO wenn man Tab drückt soll es horizontal weiter gehen nicht vertikal
 
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextField;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import util.Entry;
 
 public class Controller {
 
@@ -23,6 +28,8 @@ public class Controller {
   @FXML
   private CheckBox checkBox;
   private double garbageFactor = 0.166666667;
+  private List<Entry> entries;
+  private String xml;
 
   @FXML
   public void handleAddNewTxtField(MouseEvent mouseEvent) {
@@ -50,6 +57,8 @@ public class Controller {
   @FXML
   private void handleCalcGrade(MouseEvent mouseEvent) {
 //save all entries into xml file?
+    saveEntries();
+    /*
     double sumEcts = 0;
     double ectsWGrade = 0;
     for (int i = 0; i < ectsBox.getChildren().size() - 1; i++) {
@@ -59,26 +68,37 @@ public class Controller {
       }
       //If an entry has a grade (instead of "passed")
       TextField noteTxt = (TextField) noteBox.getChildren().get(i);
-      if (!noteTxt.getText().isEmpty() || isInteger(noteTxt.getText())) {
+      if (!noteTxt.getText().isEmpty() || isNote(noteTxt.getText())) {
         ectsWGrade += Integer.parseInt(ectsTxt.getText());
       }
     }
     if (checkBox.isSelected()) {
       double garbageECTS = Math.round((sumEcts * garbageFactor) * 100.00) / 100.00;
       discountGarbageECTS(garbageECTS);
-
     }
-
+*/
 
   }
 
   private boolean isInteger(String number) {
     try {
       int validNumber = Integer.parseInt(number);
+      if (validNumber >= 0) {
+        return true;
+      }
+    } catch (NumberFormatException e) {
+    }
+    return false;
+  }
+
+  private boolean isNote(String number) {
+    try {
+      double validNumber = Double.valueOf(number);
       if (validNumber > 0) {
         return true;
       }
     } catch (NumberFormatException e) {
+
     }
     return false;
   }
@@ -88,8 +108,8 @@ public class Controller {
     while (garbageECTS > 0) {
       for (int i = 0; i < noteBox.getChildren().size() - 1; i++) {
         TextField noteTxt = (TextField) noteBox.getChildren().get(i);
-        if (isInteger(noteTxt.getText())) {
-          int note = Integer.parseInt(noteTxt.getText());
+        if (isNote(noteTxt.getText())) {
+          double note = Double.valueOf(noteTxt.getText());
           if (note > max) {
             max = note;
 
@@ -100,7 +120,26 @@ public class Controller {
     }
   }
 
+  //TODO note kann nicht in int geparsed werden!!!
+
   private void saveEntries() {
+    entries = new ArrayList<>();
+    XStream xStream = new XStream(new DomDriver());
+    for (int i = 0; i < vorlesungBox.getChildren().size() - 1; i++) {
+      TextField vorTxt = (TextField) vorlesungBox.getChildren().get(i);
+      TextField ectsTxt = (TextField) ectsBox.getChildren().get(i);
+      TextField noteTxt = (TextField) noteBox.getChildren().get(i);
+      if (!vorTxt.getText().isEmpty() && isNote(noteTxt.getText()) && (
+          isInteger(ectsTxt.getText()) || ectsTxt.getText()
+              .isEmpty())) {
+        Entry entry = new Entry(vorTxt.getText(), Double.valueOf(noteTxt.getText()),
+            Integer.parseInt(ectsTxt.getText()));
+        entries.add(entry);
+      }
+    }
+    xStream.alias("Vorlesungen", List.class);
+    xStream.alias("Eintrag", util.Entry.class);
+    xml = xStream.toXML(entries);
 
   }
 
